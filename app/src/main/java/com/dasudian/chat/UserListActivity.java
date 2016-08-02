3417f6a886fba40f204a9a79f8f75b1bc892a094
DevEasy <cn09876@gmail.com>
2016-08-02 17:05:48 +0800

    add some test demo

--- app/src/main/java/com/dasudian/chat/UserListActivity.java
+++ app/src/main/java/com/dasudian/chat/UserListActivity.java
@@ -3,11 +3,19 @@ package com.dasudian.chat;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import com.dasudian.utils.SwHttp;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.TextHttpResponseHandler;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -19,33 +27,53 @@ public class UserListActivity extends Activity {
     {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_userlist);
+
         RequestParams params=new RequestParams();
-        Gson g=new Gson();
-
-
-
         params.add("aaa","bbb");
+
+        SwHttp.get("", null, new TextHttpResponseHandler() {
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String responseString) {
+
+            }
+        });
+
 
         SwHttp.get("books.php", params, new AsyncHttpResponseHandler()
         {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody)
             {
-
+                String s=new String(responseBody);
+                Log.e("xxx","swhttp.success "+statusCode+" "+s);
+                Gson g=new Gson();
+                List<Map<String,String>> arr=new ArrayList<>();
+                arr=g.fromJson(s,new TypeToken<ArrayList<Map<String,String>>>(){}.getType());
+                for(Map<String,String> m:arr)
+                {
+                    Log.e("xx",m.get("name"));
+                }
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error)
             {
-
+                Log.e("xxx","swhttp.fail "+statusCode+" ");
             }
 
             @Override
             public void onProgress(long bytesWritten, long totalSize)
             {
-
+                Log.e("xxx","swhttp.onprogress "+bytesWritten+"/"+totalSize);
             }
         });
+
+
 	}
 	
 	public void onClick1(View view) {
